72830dd93c8563607e971083bfaac4feed0196a8
Alex <alex.passin97@gmail.com>
2022-08-16 23:15:39 +0200

    Added more Data

--- app/src/main/java/com/frametrap/app/MoveModel.kt
+++ app/src/main/java/com/frametrap/app/MoveModel.kt
@@ -1,10 +1,10 @@
 package com.frametrap.app
 
-class MoveModel(private val prop_movename: String, private val prop_startup: String, private val prop_active: String, private val prop_recovery: String, private val prop_onBlock: String, private val prop_movetype: String) {
+class MoveModel(private val prop_movename: String, private val prop_movetype: String, private val prop_startup: String, private val prop_active: String, private val prop_recovery: String, private val prop_onBlock: String) {
     var movename: String = this.prop_movename
+    var movetype: String = this.prop_movetype
     var startup: String = this.prop_startup
     var active: String = this.prop_active
     var recovery: String = this.prop_recovery
     var onBlock: String = this.prop_onBlock
-    var movetype: String = this.prop_movetype
 }
